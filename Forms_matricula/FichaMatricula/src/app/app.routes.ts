900de939413ea02a2f3c2ab2b13dae023ff9a400
Gabriel <Gabrielferrari661@gmail.com>
2026-03-28 23:46:56 -0300

    Trabalho 2 DW2

--- Forms_matricula/FichaMatricula/src/app/app.routes.ts
+++ Forms_matricula/FichaMatricula/src/app/app.routes.ts
@@ -1,5 +1,2 @@
 import { Routes } from '@angular/router';
-import { Ficha } from './Components/ficha/ficha';
-import { NotFound } from './Components/not-found/not-found';
-
 export const routes: Routes = [];
